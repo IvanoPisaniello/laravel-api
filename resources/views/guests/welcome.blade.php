0ea40e284ab7c35ffb9bdc648ed3c99ace7e8934
IvanoPisaniello <ivanop92@gmail.com>
2023-10-08 16:37:11 +0200

    fixed home

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="jumbotron jumbotron-fluid bg-success text-white text-center py-5">
+<div class="jumbotron jumbotron-fluid transparent-card text-white text-center py-5">
     <div class="container">
         <h1 class="display-4">Ciao, sono Ivano Pisaniello</h1>
         <p class="lead">Benvenuto nel mio portfolio di progetti</p>
