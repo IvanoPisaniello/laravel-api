7e68323c2ebc90f0f3dad439e6b5f907ec860d04
IvanoPisaniello <ivanop92@gmail.com>
2023-10-12 15:27:56 +0200

    added table migration and seeder

---
+++ database/seeders/TecnologiesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TecnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $technologies = [
+            'Vue' => '#42b883',
+            'React' => '#61dafb',
+            'JS' => '#f7df1e',
+            'TS' => '#3178c6',
+            'CSS' => '#563d7c',
+            'SCSS' => '#c6538c',
+            'Bootstrap' => '#7952b3',
+            'Tailwind' => '#38a169',
+            'PHP' => '#777bb4',
+            'Laravel' => '#ff2d20',
+            'NodeJs' => '#68a063',
+        ];
+
+        foreach ($technologies as $name => $color) {
+            $new_technology = new Technology();
+            $new_technology->name = $name;
+            $new_technology->color = $color;
+            $new_technology->save();
+        }
+    }
+}
